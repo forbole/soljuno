0e10ce76d4e8dab20a3c0c9be24ba866debb7ed3
Paul <p22626262@gmail.com>
2021-08-18 18:05:42 +0800

    Implement basic client proxy

--- client/client.go
+++ client/client.go
@@ -2,191 +2,177 @@ package client
 
 import (
 	"context"
-	"encoding/hex"
-	"fmt"
-	"time"
 
-	"github.com/cosmos/cosmos-sdk/simapp/params"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/tx"
+	"github.com/desmos-labs/soljuno/types"
 
-	"github.com/desmos-labs/juno/types"
-
-	"google.golang.org/grpc"
-
-	tmjson "github.com/tendermint/tendermint/libs/json"
-	rpcclient "github.com/tendermint/tendermint/rpc/client"
-	httpclient "github.com/tendermint/tendermint/rpc/client/http"
-	tmctypes "github.com/tendermint/tendermint/rpc/core/types"
-
-	constypes "github.com/tendermint/tendermint/consensus/types"
+	client "github.com/desmos-labs/soljuno/solana/client"
+	clienttype "github.com/desmos-labs/soljuno/solana/client/types"
 )
 
-// Proxy implements a wrapper around both a Tendermint RPC client and a
+type Proxy interface {
+	// Block queries for a block by slot. An error is returned if the query fails.
+	Block(uint64) (clienttype.Block, error)
+
+	// LatestSlot returns the latest slot on the active chain. An error
+	// is returned if the query fails.
+	LatestSlot() (uint64, error)
+
+	// Slots returns the slot of confirmed blocks between the given start and end on the active chain.
+	// An error is returned if the query fails.
+	Slots(uint64, uint64) ([]uint64, error)
+}
+
+// proxy implements a wrapper around both a Tendermint RPC client and a
 // Cosmos Sdk REST client that allows for essential data queries.
-type Proxy struct {
-	ctx            context.Context
-	encodingConfig *params.EncodingConfig
+type proxy struct {
+	ctx context.Context
 
-	rpcClient rpcclient.Client
-
-	grpConnection   *grpc.ClientConn
-	txServiceClient tx.ServiceClient
+	rpcClient client.Client
 }
 
 // NewClientProxy allows to build a new Proxy instance
-func NewClientProxy(cfg types.Config, encodingConfig *params.EncodingConfig) (*Proxy, error) {
-	rpcClient, err := httpclient.New(cfg.GetRPCConfig().GetAddress(), "/websocket")
-	if err != nil {
-		return nil, err
-	}
+func NewClientProxy(cfg types.Config) (Proxy, error) {
+	rpcClient := client.NewClient(cfg.GetRPCConfig().GetAddress())
 
-	if err := rpcClient.Start(); err != nil {
-		return nil, err
-	}
-
-	grpcConnection, err := CreateGrpcConnection(cfg)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Proxy{
-		encodingConfig:  encodingConfig,
-		ctx:             context.Background(),
-		rpcClient:       rpcClient,
-		grpConnection:   grpcConnection,
-		txServiceClient: tx.NewServiceClient(grpcConnection),
+	return &proxy{
+		ctx:       context.Background(),
+		rpcClient: rpcClient,
 	}, nil
 }
 
-// LatestHeight returns the latest block height on the active chain. An error
-// is returned if the query fails.
-func (cp *Proxy) LatestHeight() (int64, error) {
-	status, err := cp.rpcClient.Status(cp.ctx)
+func (cp *proxy) LatestSlot() (uint64, error) {
+	slot, err := cp.rpcClient.GetSlot()
 	if err != nil {
-		return -1, err
+		return 0, err
 	}
 
-	height := status.SyncInfo.LatestBlockHeight
-	return height, nil
+	return slot, nil
 }
 
-// Block queries for a block by height. An error is returned if the query fails.
-func (cp *Proxy) Block(height int64) (*tmctypes.ResultBlock, error) {
-	return cp.rpcClient.Block(cp.ctx, &height)
-}
-
-// TendermintTx queries for a transaction by hash. An error is returned if the
-// query fails.
-func (cp *Proxy) TendermintTx(hash string) (*tmctypes.ResultTx, error) {
-	hashRaw, err := hex.DecodeString(hash)
+func (cp *proxy) Slots(start uint64, end uint64) ([]uint64, error) {
+	slots, err := cp.rpcClient.GetBlocks(start, end)
 	if err != nil {
-		return nil, err
+		return []uint64{}, err
 	}
 
-	return cp.rpcClient.Tx(cp.ctx, hashRaw, false)
+	return slots, nil
 }
 
-// Validators returns all the known Tendermint validators for a given block
-// height. An error is returned if the query fails.
-func (cp *Proxy) Validators(height int64) (*tmctypes.ResultValidators, error) {
-	vals := &tmctypes.ResultValidators{
-		BlockHeight: height,
-	}
-
-	page := 1
-	stop := false
-	for !stop {
-		result, err := cp.rpcClient.Validators(cp.ctx, &height, &page, nil)
-		if err != nil {
-			return nil, err
-		}
-		vals.Validators = append(vals.Validators, result.Validators...)
-		vals.Count += result.Count
-		vals.Total = result.Total
-
-		page += 1
-		stop = vals.Count == vals.Total
-	}
-
-	return vals, nil
+func (cp *proxy) Block(slot uint64) (clienttype.Block, error) {
+	return cp.rpcClient.GetBlock(slot)
 }
 
-// Genesis returns the genesis state
-func (cp *Proxy) Genesis() (*tmctypes.ResultGenesis, error) {
-	return cp.rpcClient.Genesis(cp.ctx)
-}
+// // TendermintTx queries for a transaction by hash. An error is returned if the
+// // query fails.
+// func (cp *Proxy) TendermintTx(hash string) (*tmctypes.ResultTx, error) {
+// 	hashRaw, err := hex.DecodeString(hash)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-// ConsensusState returns the consensus state of the chain
-func (cp *Proxy) ConsensusState() (*constypes.RoundStateSimple, error) {
-	state, err := cp.rpcClient.ConsensusState(context.Background())
-	if err != nil {
-		return nil, err
-	}
+// 	return cp.rpcClient.Tx(cp.ctx, hashRaw, false)
+// }
 
-	var data constypes.RoundStateSimple
-	err = tmjson.Unmarshal(state.RoundState, &data)
-	if err != nil {
-		return nil, err
-	}
-	return &data, nil
-}
+// // Validators returns all the known Tendermint validators for a given block
+// // height. An error is returned if the query fails.
+// func (cp *Proxy) Validators(height int64) (*tmctypes.ResultValidators, error) {
+// 	vals := &tmctypes.ResultValidators{
+// 		BlockHeight: height,
+// 	}
 
-// SubscribeEvents subscribes to new events with the given query through the RPC
-// client with the given subscriber name. A receiving only channel, context
-// cancel function and an error is returned. It is up to the caller to cancel
-// the context and handle any errors appropriately.
-func (cp *Proxy) SubscribeEvents(subscriber, query string) (<-chan tmctypes.ResultEvent, context.CancelFunc, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	eventCh, err := cp.rpcClient.Subscribe(ctx, subscriber, query)
-	return eventCh, cancel, err
-}
+// 	page := 1
+// 	stop := false
+// 	for !stop {
+// 		result, err := cp.rpcClient.Validators(cp.ctx, &height, &page, nil)
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 		vals.Validators = append(vals.Validators, result.Validators...)
+// 		vals.Count += result.Count
+// 		vals.Total = result.Total
 
-// SubscribeNewBlocks subscribes to the new block event handler through the RPC
-// client with the given subscriber name. An receiving only channel, context
-// cancel function and an error is returned. It is up to the caller to cancel
-// the context and handle any errors appropriately.
-func (cp *Proxy) SubscribeNewBlocks(subscriber string) (<-chan tmctypes.ResultEvent, context.CancelFunc, error) {
-	return cp.SubscribeEvents(subscriber, "tm.event = 'NewBlock'")
-}
+// 		page += 1
+// 		stop = vals.Count == vals.Total
+// 	}
 
-// Tx queries for a transaction from the REST client and decodes it into a sdk.Tx
-// if the transaction exists. An error is returned if the tx doesn't exist or
-// decoding fails.
-func (cp *Proxy) Tx(hash string) (*sdk.TxResponse, *tx.Tx, error) {
-	res, err := cp.txServiceClient.GetTx(context.Background(), &tx.GetTxRequest{Hash: hash})
-	if err != nil {
-		return nil, nil, err
-	}
-	return res.TxResponse, res.Tx, nil
-}
+// 	return vals, nil
+// }
 
-// Txs queries for all the transactions in a block. Transactions are returned
-// in the sdk.TxResponse format which internally contains an sdk.Tx. An error is
-// returned if any query fails.
-func (cp *Proxy) Txs(block *tmctypes.ResultBlock) ([]*types.Tx, error) {
-	txResponses := make([]*types.Tx, len(block.Block.Txs))
-	for i, tmTx := range block.Block.Txs {
-		txResponse, txObj, err := cp.Tx(fmt.Sprintf("%X", tmTx.Hash()))
-		if err != nil {
-			return nil, err
-		}
+// // Genesis returns the genesis state
+// func (cp *Proxy) Genesis() (*tmctypes.ResultGenesis, error) {
+// 	return cp.rpcClient.Genesis(cp.ctx)
+// }
 
-		convTx, err := types.NewTx(txResponse, txObj)
-		if err != nil {
-			return nil, fmt.Errorf("error converting transaction: %s", err.Error())
-		}
+// // ConsensusState returns the consensus state of the chain
+// func (cp *Proxy) ConsensusState() (*constypes.RoundStateSimple, error) {
+// 	state, err := cp.rpcClient.ConsensusState(context.Background())
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-		txResponses[i] = convTx
-	}
+// 	var data constypes.RoundStateSimple
+// 	err = tmjson.Unmarshal(state.RoundState, &data)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return &data, nil
+// }
 
-	return txResponses, nil
-}
+// // SubscribeEvents subscribes to new events with the given query through the RPC
+// // client with the given subscriber name. A receiving only channel, context
+// // cancel function and an error is returned. It is up to the caller to cancel
+// // the context and handle any errors appropriately.
+// func (cp *Proxy) SubscribeEvents(subscriber, query string) (<-chan tmctypes.ResultEvent, context.CancelFunc, error) {
+// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+// 	eventCh, err := cp.rpcClient.Subscribe(ctx, subscriber, query)
+// 	return eventCh, cancel, err
+// }
 
-// Stop defers the node stop execution to the RPC client.
-func (cp *Proxy) Stop() {
-	err := cp.rpcClient.Stop()
-	if err != nil {
-		panic(fmt.Errorf("error while stopping proxy: %s", err))
-	}
-}
+// // SubscribeNewBlocks subscribes to the new block event handler through the RPC
+// // client with the given subscriber name. An receiving only channel, context
+// // cancel function and an error is returned. It is up to the caller to cancel
+// // the context and handle any errors appropriately.
+// func (cp *Proxy) SubscribeNewBlocks(subscriber string) (<-chan tmctypes.ResultEvent, context.CancelFunc, error) {
+// 	return cp.SubscribeEvents(subscriber, "tm.event = 'NewBlock'")
+// }
+
+// // Tx queries for a transaction from the REST client and decodes it into a sdk.Tx
+// // if the transaction exists. An error is returned if the tx doesn't exist or
+// // decoding fails.
+// func (cp *Proxy) Tx(hash string) (*sdk.TxResponse, *tx.Tx, error) {
+// 	res, err := cp.txServiceClient.GetTx(context.Background(), &tx.GetTxRequest{Hash: hash})
+// 	if err != nil {
+// 		return nil, nil, err
+// 	}
+// 	return res.TxResponse, res.Tx, nil
+// }
+
+// // Txs queries for all the transactions in a block. Transactions are returned
+// // in the sdk.TxResponse format which internally contains an sdk.Tx. An error is
+// // returned if any query fails.
+// func (cp *Proxy) Txs(block *tmctypes.ResultBlock) ([]*types.Tx, error) {
+// 	txResponses := make([]*types.Tx, len(block.Block.Txs))
+// 	for i, tmTx := range block.Block.Txs {
+// 		txResponse, txObj, err := cp.Tx(fmt.Sprintf("%X", tmTx.Hash()))
+// 		if err != nil {
+// 			return nil, err
+// 		}
+
+// 		convTx, err := types.NewTx(txResponse, txObj)
+// 		if err != nil {
+// 			return nil, fmt.Errorf("error converting transaction: %s", err.Error())
+// 		}
+
+// 		txResponses[i] = convTx
+// 	}
+
+// 	return txResponses, nil
+// }
+
+// // Stop defers the node stop execution to the RPC client.
+// func (cp *Proxy) Stop() {
+// 	err := cp.rpcClient.Stop()
+// 	if err != nil {
+// 		panic(fmt.Errorf("error while stopping proxy: %s", err))
+// 	}
+// }
--- solana/client/client.go
+++ solana/client/client.go
@@ -5,24 +5,48 @@ import (
 	jsonrpc "github.com/ybbus/jsonrpc/v2"
 )
 
-type Client struct {
+type Client interface {
+	GetSlot() (uint64, error)
+	GetBlock(uint64) (types.Block, error)
+	GetBlocks(uint64, uint64) ([]uint64, error)
+}
+
+type client struct {
 	endpoint  string
 	rpcClient jsonrpc.RPCClient
 }
 
-func NewClient(endpoint string) *Client {
+func NewClient(endpoint string) *client {
 	rpcClient := jsonrpc.NewClient(endpoint)
-	return &Client{
+	return &client{
 		endpoint:  endpoint,
 		rpcClient: rpcClient,
 	}
 }
 
-func (c *Client) Block(slot uint64) (types.Block, error) {
+func (c *client) GetBlock(slot uint64) (types.Block, error) {
 	var block types.Block
 	err := c.rpcClient.CallFor(&block, "getBlock", slot)
 	if err != nil {
 		return block, err
 	}
 	return block, nil
+}
+
+func (c *client) GetSlot() (uint64, error) {
+	var slot uint64
+	err := c.rpcClient.CallFor(&slot, "getSlot")
+	if err != nil {
+		return slot, err
+	}
+	return slot, nil
+}
+
+func (c *client) GetBlocks(start uint64, end uint64) ([]uint64, error) {
+	var slots []uint64
+	err := c.rpcClient.CallFor(&slots, "getBlocks", start, end)
+	if err != nil {
+		return slots, err
+	}
+	return slots, nil
 }
