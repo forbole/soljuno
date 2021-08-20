d621b704144f1943e4937ecfe9a860edb41ad2cb
Paul <p22626262@gmail.com>
2021-08-20 17:58:55 +0800

    Add bpf loader instructions

--- solana/program/bpfloader/instruction.go
+++ solana/program/bpfloader/instruction.go
@@ -3,6 +3,13 @@ package bpfloader
 type LoaderInstruction uint32
 
 const (
+	// Write program data into an account
 	Write LoaderInstruction = iota
 	Finalize
 )
+
+// Finalize an account loaded with program data for execution
+type WriteInstruction struct {
+	Offset uint32
+	Bytes  []byte
+}
