9b40a17aa202c53c0cdb9c3e1da45c9f83adafb9
Paul <p22626262@gmail.com>
2021-09-03 19:25:58 +0800

    Add updagrable bpf loader program parser

---
+++ solana/program/bpfloader/upgradeable/id.go
@@ -0,0 +1,3 @@
+package upgradable_loader
+
+const ProgramID = "BPFLoaderUpgradeab1e11111111111111111111111"
---
+++ solana/program/bpfloader/upgradeable/parse.go
@@ -0,0 +1,76 @@
+package upgradable_loader
+
+import (
+	"github.com/forbole/soljuno/solana/bincode"
+	"github.com/forbole/soljuno/solana/parser"
+	"github.com/forbole/soljuno/solana/types"
+)
+
+type Parser struct {
+	parser.ProgramParser
+}
+
+func (Parser) Parse(accounts []string, data []byte) types.ParsedInstruction {
+	decoder := bincode.NewDecoder()
+	var id InstructionID
+	decoder.Decode(data[:4], &id)
+	switch id {
+	case InitializeBuffer:
+		return types.NewParsedInstruction(
+			"initializeBuffer",
+			NewParsedInitializeBuffer(
+				accounts,
+			),
+		)
+
+	case Write:
+		var instruction WriteInstruction
+		decoder.Decode(data[4:], &instruction)
+		return types.NewParsedInstruction(
+			"write",
+			NewParsedWrite(
+				accounts,
+				instruction,
+			),
+		)
+
+	case DeployWithMaxDataLen:
+		var instruction DeployWithMaxDataLenInstruction
+		decoder.Decode(data[4:], &instruction)
+		return types.NewParsedInstruction(
+			"deployWithMaxDataLen",
+			NewParsedDeployWithMaxDataLen(
+				accounts,
+				instruction,
+			),
+		)
+
+	case Upgrade:
+		return types.NewParsedInstruction(
+			"upgrade",
+			NewParsedUpgrade(
+				accounts,
+			),
+		)
+
+	case SetAuthority:
+		return types.NewParsedInstruction(
+			"setAuthority",
+			NewParsedSetAuthority(
+				accounts,
+			),
+		)
+
+	case Close:
+		return types.NewParsedInstruction(
+			"close",
+			NewParsedClose(
+				accounts,
+			),
+		)
+	}
+	return types.NewParsedInstruction(
+		"unknown",
+		nil,
+	)
+}
