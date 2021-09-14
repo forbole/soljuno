9aaade190d5c2c759d02f2ce8167e80b42cd31d3
Paul <p22626262@gmail.com>
2021-09-14 18:07:52 +0800

    Build system nonce account database

--- db/postgresql/system.go
+++ db/postgresql/system.go
@@ -1,1 +1,35 @@
 package postgresql
+
+import "github.com/forbole/soljuno/solana/types"
+
+// SaveToken implements the db.SystemDb
+func (db *Database) SaveNonce(
+	address string,
+	slot uint64,
+	authority string,
+	blockhash string,
+	feeCalculator types.FeeCalculator,
+	state string,
+) error {
+	stmt := `
+    INSERT INTO nonce
+        (address, slot, authority, blockhash, lamports_per_signature, state)
+    VALUES ($1, $2, $3, $4, $5)
+    ON CONFLICT (mint) DO UPDATE
+        SET slot = excluded.slot,
+        authority = excluded.authority,
+        blockhash = excluded.blockhash,
+        lamports_per_signature = excluded.lamports_per_signature,
+        state = excluded.state
+    WHERE nonce.slot <= excluded.slot`
+	_, err := db.Sqlx.Exec(
+		stmt,
+		address,
+		slot,
+		authority,
+		blockhash,
+		feeCalculator.LamportsPerSignature,
+		state,
+	)
+	return err
+}
--- db/schema/03-system.sql
+++ db/schema/03-system.sql
@@ -1,6 +1,7 @@
 CREATE TABLE nonce
 (
     address                 TEXT    NOT NULL PRIMARY KEY,
+    slot                    BIGINT  NOT NULL,
     authority               TEXT    NOT NULL,
     blockhash               TEXT    NOT NULL,
     lamports_per_signature  INT     NOT NULL,
