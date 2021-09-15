7fd43ffd5d30cb1aad2a194f16bd8aa5de19e559
Paul <p22626262@gmail.com>
2021-09-15 11:57:47 +0800

    Design schema

---
+++ db/schema/04-stake.sql
@@ -0,0 +1,27 @@
+CREATE TABLE stake
+(
+    address     TEXT    NOT NULL PRIMARY KEY,
+    slot        BIGINT  NOT NULL,
+    staker      TEXT    NOT NULL,
+    withdrawer  TEXT    NOT NULL
+);
+
+CREATE TABLE stake_lockup
+(
+    address         TEXT        NOT NULL PRIMARY KEY,
+    slot            BIGINT      NOT NULL,
+    custodian       TEXT        NOT NULL,
+    epoch           BIGINT      NOT NULL,
+    unix_timestamp  TIMESTAMP   WITHOUT TIME ZONE NOT NULL
+);
+
+CREATE TABLE stake_delegation
+(
+    address                 TEXT    NOT NULL PRIMARY KEY,
+    slot                    BIGINT  NOT NULL,
+    activation_epoch        BIGINT  NOT NULL,
+    deactivation_epoch      BIGINT  NOT NULL,
+    stake                   BIGINT  NOT NULL,
+    voter                   TEXT    NOT NULL,
+    warmup_cooldown_rate    FLOAT   NOT NULL
+);
