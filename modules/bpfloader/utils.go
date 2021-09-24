7cfb89b917ad04ef84058dd05c39606495500d22
Paul <p22626262@gmail.com>
2021-09-24 19:14:40 +0800

    Init utils

--- modules/bpfloader/utils.go
+++ modules/bpfloader/utils.go
@@ -5,6 +5,14 @@ import (
 	"github.com/forbole/soljuno/db"
 )
 
+func updateBufferAccount(address string, db db.BpfLoaderDb, client client.Proxy) error {
+	return nil
+}
+
 func updateProgramAccount(address string, db db.BpfLoaderDb, client client.Proxy) error {
+	return nil
+}
+
+func updateProgramDataAccount(address string, db db.BpfLoaderDb, client client.Proxy) error {
 	return nil
 }
