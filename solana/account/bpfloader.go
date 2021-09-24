ff622257040401b4fbf9e49c336423572c53f870
Paul <p22626262@gmail.com>
2021-09-24 14:27:09 +0800

    Add bpf loader account parser

---
+++ solana/account/bpfloader.go
@@ -0,0 +1,60 @@
+package account_parser
+
+import (
+	"github.com/forbole/soljuno/solana/bincode"
+	"github.com/forbole/soljuno/solana/types"
+)
+
+func bpfLoaderParse(decoder bincode.Decoder, bz []byte) interface{} {
+	var state LoaderState
+	decoder.Decode(bz[:4], &state)
+	switch state {
+	case BufferLoaderState:
+		return bufferAccountParse(decoder, bz[4:])
+	case ProgramLoaderState:
+		return programAccountParse(decoder, bz[4:])
+	case ProgramDataLoaderState:
+		return programDataAccountParse(decoder, bz[4:])
+	}
+	return nil
+}
+
+func bufferAccountParse(decoder bincode.Decoder, bz []byte) interface{} {
+	var account BufferAccount
+	decoder.Decode(bz, &account)
+	return account
+}
+
+func programAccountParse(decoder bincode.Decoder, bz []byte) interface{} {
+	var account ProgramAccount
+	decoder.Decode(bz, &account)
+	return account
+}
+
+func programDataAccountParse(decoder bincode.Decoder, bz []byte) interface{} {
+	var account ProgramDataAccount
+	decoder.Decode(bz, &account)
+	return account
+}
+
+type LoaderState uint32
+
+const (
+	UninitializedLoaderState LoaderState = iota
+	BufferLoaderState
+	ProgramLoaderState
+	ProgramDataLoaderState
+)
+
+type BufferAccount struct {
+	Authority types.Pubkey
+}
+
+type ProgramAccount struct {
+	ProgramDataAccount types.Pubkey
+}
+
+type ProgramDataAccount struct {
+	Slot      uint64
+	Authority types.Pubkey
+}
