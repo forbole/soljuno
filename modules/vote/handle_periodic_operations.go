2b14ac33b11325d06ac9f7ed42368729db7700a3
Paul <p22626262@gmail.com>
2021-10-13 18:18:11 +0800

    Add periodic validators status updating

--- modules/vote/handle_periodic_operations.go
+++ modules/vote/handle_periodic_operations.go
@@ -3,6 +3,7 @@ package vote
 import (
 	"fmt"
 
+	"github.com/forbole/soljuno/db"
 	"github.com/forbole/soljuno/modules/utils"
 	"github.com/go-co-op/gocron"
 	"github.com/rs/zerolog/log"
@@ -13,7 +14,7 @@ func (m *Module) RegisterPeriodicOperations(scheduler *gocron.Scheduler) error {
 	log.Debug().Str("module", "vote").Msg("setting up periodic tasks")
 
 	if _, err := scheduler.Every(1).Minute().Do(func() {
-		utils.WatchMethod(m.updateValidatorStatus)
+		utils.WatchMethod(m.updateValidatorsStatus)
 	}); err != nil {
 		return fmt.Errorf("error while setting up vote periodic operation: %s", err)
 	}
@@ -21,11 +22,42 @@ func (m *Module) RegisterPeriodicOperations(scheduler *gocron.Scheduler) error {
 	return nil
 }
 
-// updateValidatorStatus insert average block time in the latest minute
-func (m *Module) updateValidatorStatus() error {
-	_, _, err := m.client.ValidatorsWithSlot()
+// updateValidatorsStatus insert current validators status
+func (m *Module) updateValidatorsStatus() error {
+	voteDb, ok := m.db.(db.VoteDb)
+	if !ok {
+		return fmt.Errorf("vote is enabled, but your database does not implement VoteDb")
+	}
+
+	slot, voteAccounts, err := m.client.ValidatorsWithSlot()
 	if err != nil {
 		return nil
+	}
+
+	for _, account := range voteAccounts.Current {
+		if err := voteDb.SaveValidatorStatus(
+			account.VotePubkey,
+			slot,
+			account.ActivatedStake,
+			account.LastVote,
+			account.RootSlot,
+			true,
+		); err != nil {
+			return err
+		}
+	}
+
+	for _, account := range voteAccounts.Delinquent {
+		if err := voteDb.SaveValidatorStatus(
+			account.VotePubkey,
+			slot,
+			account.ActivatedStake,
+			account.LastVote,
+			account.RootSlot,
+			false,
+		); err != nil {
+			return err
+		}
 	}
 	return nil
 }
