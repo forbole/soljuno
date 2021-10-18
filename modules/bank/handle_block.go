f2e1924b0ea3ba795f4b1213dd2719bedf524341
Paul <p22626262@gmail.com>
2021-10-18 17:30:09 +0800

    Fix rewards duplicate conflict in one query problem

--- modules/bank/handle_block.go
+++ modules/bank/handle_block.go
@@ -8,9 +8,16 @@ import (
 func HandleBlock(block types.Block, db db.BankDb) error {
 	var accounts []string
 	var balances []uint64
-	for _, reward := range block.Rewards {
+	m := make(map[string]int)
+	for i, reward := range block.Rewards {
+		if preId, ok := m[reward.Pubkey]; ok {
+			balances[preId] = reward.PostBalance
+			m[reward.Pubkey] = i
+			continue
+		}
 		accounts = append(accounts, reward.Pubkey)
 		balances = append(balances, reward.PostBalance)
+		m[reward.Pubkey] = i
 	}
 	return db.SaveAccountBalances(block.Slot, accounts, balances)
 }
