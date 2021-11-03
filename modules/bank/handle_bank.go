9db571aae868a8a6a7bccad590eeb00fb330a57a
Paul <p22626262@gmail.com>
2021-11-03 17:45:38 +0800

    Fix deadlock in bank module (#46)
    * Change all bank events to block module
    
    * Fix bank module by sia specific queue with single comsumer
    
    * Add comments
    
    * Update worker/types.go
    
    * Update worker/types.go

---
+++ modules/bank/handle_bank.go
@@ -0,0 +1,64 @@
+package bank
+
+import (
+	"github.com/forbole/soljuno/db"
+	clienttypes "github.com/forbole/soljuno/solana/client/types"
+	"github.com/forbole/soljuno/types"
+)
+
+func HandleBank(block types.Block, db db.BankDb) error {
+	var accounts []string
+	var balances []uint64
+	var tokenAccounts []string
+	var tokenBalances []clienttypes.TransactionTokenBalance
+	accountMap := make(map[string]int)
+	tokenAccountMap := make(map[string]int)
+
+	accountIndex := 0
+	// Gather balances from rewards
+	for _, reward := range block.Rewards {
+		if index, ok := accountMap[reward.Pubkey]; ok {
+			balances[index] = reward.PostBalance
+			continue
+		}
+		accounts = append(accounts, reward.Pubkey)
+		balances = append(balances, reward.PostBalance)
+		accountMap[reward.Pubkey] = accountIndex
+		accountIndex++
+	}
+
+	tokenAccountIndex := 0
+	for _, tx := range block.Txs {
+		// Gather balances from tx
+		for i, pubkey := range tx.Accounts {
+			if index, ok := accountMap[pubkey]; ok {
+				balances[index] = tx.PostBalances[i]
+				continue
+			}
+			accounts = append(accounts, pubkey)
+			balances = append(balances, tx.PostBalances[i])
+			accountMap[pubkey] = accountIndex
+			accountIndex++
+		}
+
+		// Gather token balances from tx
+		for _, balance := range tx.PostTokenBalances {
+			pubkey := tx.Accounts[balance.AccountIndex]
+			if index, ok := tokenAccountMap[pubkey]; ok {
+				tokenBalances[index].UiTokenAmount = balance.UiTokenAmount
+				tokenBalances[index].Mint = balance.Mint
+				continue
+			}
+			balance.AccountIndex = uint(tokenAccountIndex)
+			tokenBalances = append(tokenBalances, balance)
+			tokenAccounts = append(tokenAccounts, pubkey)
+			tokenAccountMap[pubkey] = tokenAccountIndex
+			tokenAccountIndex++
+		}
+	}
+	err := db.SaveAccountBalances(block.Slot, accounts, balances)
+	if err != nil {
+		return err
+	}
+	return db.SaveAccountTokenBalances(block.Slot, tokenAccounts, tokenBalances)
+}
--- types/queue.go
+++ types/queue.go
@@ -6,3 +6,10 @@ type SlotQueue chan uint64
 func NewQueue(size int) SlotQueue {
 	return make(chan uint64, size)
 }
+
+// BankTaskQueue is a simple type alias for a (buffered) channel of bank tasks.
+type BankTaskQueue chan func()
+
+func NewBankTaskQueue(size int) BankTaskQueue {
+	return make(chan func(), size)
+}
