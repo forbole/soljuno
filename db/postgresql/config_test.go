1b3c49a02e2e1907f770b467280f42a7c82cb1f8
Paul <p22626262@gmail.com>
2021-11-12 15:30:26 +0800

    Revise config database (#59)

--- db/postgresql/config_test.go
+++ db/postgresql/config_test.go
@@ -1,11 +1,23 @@
 package postgresql_test
 
+import "encoding/json"
+
 func (suite *DbTestSuite) TestSaveConfigAccount() {
 	type ConfigAccountRow struct {
-		Address string `db:"address"`
-		Slot    uint64 `db:"slot"`
-		Owner   string `db:"owner"`
-		Value   string `db:"value"`
+		Address         string `db:"address"`
+		Slot            uint64 `db:"slot"`
+		Owner           string `db:"owner"`
+		Name            string `db:"name"`
+		KeybaseUsername string `db:"keybase_username"`
+		Website         string `db:"website"`
+		Details         string `db:"details"`
+	}
+
+	type parsedInfo struct {
+		Name            string `json:"name"`
+		KeybaseUsername string `json:"keybaseUsername"`
+		Website         string `json:"website"`
+		Details         string `json:"details"`
 	}
 
 	testCases := []struct {
@@ -16,37 +28,37 @@ func (suite *DbTestSuite) TestSaveConfigAccount() {
 		{
 			name: "initialize the data",
 			data: ConfigAccountRow{
-				"address", 1, "owner", `{"details": "details"}`,
+				"address", 1, "owner", "name", "", "website", "details",
 			},
 			expected: ConfigAccountRow{
-				"address", 1, "owner", `{"details": "details"}`,
+				"address", 1, "owner", "name", "", "website", "details",
 			},
 		},
 		{
 			name: "update with lower slot",
 			data: ConfigAccountRow{
-				"address", 0, "owner", `{"details": "pre_details"}`,
+				"address", 0, "owner", "name", "", "website", "pre_details",
 			},
 			expected: ConfigAccountRow{
-				"address", 1, "owner", `{"details": "details"}`,
+				"address", 1, "owner", "name", "", "website", "details",
 			},
 		},
 		{
 			name: "update with same slot",
 			data: ConfigAccountRow{
-				"address", 1, "owner", `{"details": "curr_details"}`,
+				"address", 1, "owner", "name", "", "website", "curr_details",
 			},
 			expected: ConfigAccountRow{
-				"address", 1, "owner", `{"details": "curr_details"}`,
+				"address", 1, "owner", "name", "", "website", "curr_details",
 			},
 		},
 		{
 			name: "update with higher slot",
 			data: ConfigAccountRow{
-				"address", 2, "owner", `{"details": "new_details"}`,
+				"address", 2, "owner", "name", "", "website", "new_details",
 			},
 			expected: ConfigAccountRow{
-				"address", 2, "owner", `{"details": "new_details"}`,
+				"address", 2, "owner", "name", "", "website", "new_details",
 			},
 		},
 	}
@@ -54,17 +66,25 @@ func (suite *DbTestSuite) TestSaveConfigAccount() {
 	for _, tc := range testCases {
 		tc := tc
 		suite.Run(tc.name, func() {
-			err := suite.database.SaveConfigAccount(
+			value, err := json.Marshal(
+				parsedInfo{
+					tc.data.Name,
+					tc.data.KeybaseUsername,
+					tc.data.Website,
+					tc.data.Details,
+				},
+			)
+			err = suite.database.SaveConfigAccount(
 				tc.data.Address,
 				tc.data.Slot,
 				tc.data.Owner,
-				tc.data.Value,
+				string(value),
 			)
 			suite.Require().NoError(err)
 
 			// Verify the data
 			rows := []ConfigAccountRow{}
-			err = suite.database.Sqlx.Select(&rows, "SELECT * FROM config_account")
+			err = suite.database.Sqlx.Select(&rows, "SELECT * FROM validator_config")
 			suite.Require().NoError(err)
 			suite.Require().Len(rows, 1)
 			suite.Require().Equal(tc.expected, rows[0])
--- db/schema/06-config.sql
+++ db/schema/06-config.sql
@@ -1,8 +1,12 @@
-CREATE TABLE config_account
+CREATE TABLE validator_config
 (
-    address     TEXT    NOT NULL PRIMARY KEY,
-    slot        BIGINT  NOT NULL,
-    owner       TEXT    NOT NULL,
-    value       JSONB   NOT NULL DEFAULT '{}'::JSONB
+    address             TEXT    NOT NULL PRIMARY KEY,
+    slot                BIGINT  NOT NULL,
+    owner               TEXT    NOT NULL,
+    name                TEXT    NOT NULL DEFAULT '',
+    keybase_username    TEXT    NOT NULL DEFAULT '',
+    website             TEXT    NOT NULL DEFAULT '',
+    details             TEXT    NOT NULL DEFAULT ''
+
 );
-CREATE INDEX config_account_owner_index ON config_account (owner);
+CREATE INDEX validator_config_owner_index ON validator_config (owner);
