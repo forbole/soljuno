eb9df86acac1e5f28ec9c881cfb91750defad224
Paul <p22626262@gmail.com>
2021-11-17 14:40:09 +0800

    Enable hasura (#60)
    * Init hasura config
    
    * Add basic, bank and token modules
    
    * Fix select perissions
    
    * Add system and stake modules
    
    * Fix permission
    
    * Correct hasura config
    
    * Add vote modules
    
    * Fix token relationship
    
    * Add validator config
    
    * Add program accounts
    
    * Remove slot column in hasura
    
    * Add functions
    
    * Add functions

--- db/schema/00-solana.sql
+++ db/schema/00-solana.sql
@@ -8,7 +8,6 @@ CREATE TABLE block
 CREATE INDEX block_hash_index ON block (hash);
 CREATE INDEX block_proposer_index ON block (proposer);
 
-
 CREATE TABLE transaction
 (
     hash       TEXT     NOT NULL PRIMARY KEY,
@@ -29,6 +28,26 @@ CREATE TABLE message
     value               JSONB   NOT NULL DEFAULT '{}'::JSONB
 );
 CREATE INDEX message_transaction_hash_index ON message (transaction_hash);
+
+/**
+ * This function is used to find all the utils that involve any of the given addresses and have
+ * type that is one of the specified types.
+ */
+CREATE FUNCTION messages_by_address(
+    addresses TEXT[],
+    types TEXT[],
+    "limit" BIGINT = 100,
+    "offset" BIGINT = 0)
+    RETURNS SETOF message AS
+$$
+SELECT message.transaction_hash, message.index, message.program, message.involved_accounts, message.type, message.value
+FROM message
+         JOIN transaction t on message.transaction_hash = t.hash
+WHERE (cardinality(types) = 0 OR type = ANY (types))
+  AND addresses && involved_accounts
+ORDER BY slot DESC
+LIMIT "limit" OFFSET "offset"
+$$ LANGUAGE sql STABLE;
 
 CREATE TABLE pruning
 (
