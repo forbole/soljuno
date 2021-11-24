eaabf3ed2fc6425e4b9ff52589995a2624cb1a04
Paul <p22626262@gmail.com>
2021-11-24 17:17:53 +0800

    Fix price table

--- db/schema/08-prices.sql
+++ db/schema/08-prices.sql
@@ -6,9 +6,10 @@ CREATE TABLE token_unit
 
 CREATE TABLE token_price
 (
-    unit_name  TEXT                        NOT NULL REFERENCES token_unit (token_name) UNIQUE,
+    unit_name  TEXT                        NOT NULL REFERENCES token_unit (token_name),
     price      DECIMAL                     NOT NULL,
     market_cap BIGINT                      NOT NULL,
     timestamp  TIMESTAMP WITHOUT TIME ZONE NOT NULL
 );
+CREATE INDEX token_price_unit_name_index ON token_price (unit_name);
 CREATE INDEX token_price_timestamp_index ON token_price (timestamp);
