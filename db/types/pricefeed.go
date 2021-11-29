3555423fcd9c904e3e9e944e401d13a19df4840e
Paul <p22626262@gmail.com>
2021-11-29 14:32:36 +0800

    Enable price feed hasura (#79)
    * Init price feed module
    
    * Fix price table
    
    * Rename filename
    
    * Build pricefeed module
    
    * Enable hasura
    
    * Fix token snapshot update

--- db/types/pricefeed.go
+++ db/types/pricefeed.go
@@ -1,7 +1,7 @@
 package types
 
 type TokenUnitRow struct {
-	TokenName string `db:"token_name"`
+	TokenName string `db:"unit_name"`
 	Address   string `db:"address"`
 	PriceID   string `db:"price_id"`
 }
