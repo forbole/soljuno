877a021e2330240eb60dda1342e6baeae05337a1
Paul <p22626262@gmail.com>
2021-12-07 15:28:52 +0800

    Update validator config

--- db/postgresql/config.go
+++ db/postgresql/config.go
@@ -11,15 +11,16 @@ var _ db.ConfigDb = &Database{}
 func (db *Database) SaveValidatorConfig(row dbtypes.ValidatorConfigRow) error {
 	stmt := `
 INSERT INTO validator_config
-    (address, slot, owner, name, keybase_username, website, details)
-VALUES ($1, $2, $3, $4, $5, $6, $7)
+    (address, slot, owner, name, keybase_username, website, details, avatar_url)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
 ON CONFLICT (address) DO UPDATE
     SET slot = excluded.slot,
     owner = excluded.owner,
 	name = excluded.name,
 	keybase_username = excluded.keybase_username,
 	website = excluded.website,
-	details = excluded.details
+	details = excluded.details,
+	avatar_url = excluded.avatar_url
 WHERE validator_config.slot <= excluded.slot`
 	_, err := db.Sqlx.Exec(
 		stmt,
@@ -30,6 +31,7 @@ WHERE validator_config.slot <= excluded.slot`
 		row.KeybaseUsername,
 		row.Website,
 		row.Details,
+		row.AvatarURL,
 	)
 	return err
 }
