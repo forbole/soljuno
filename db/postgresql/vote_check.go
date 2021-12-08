9c8c82c1150d9d780ccc423ceecf71a7996c89e5
Paul <p22626262@gmail.com>
2021-12-08 11:25:34 +0800

    Rename vote account table into validator

--- db/postgresql/vote_check.go
+++ db/postgresql/vote_check.go
@@ -3,7 +3,7 @@ package postgresql
 // CheckStakeAccountLatest implements the db.StakeCheckerDb
 // if error, it returns true since there is no address statement inside database
 func (db *Database) CheckVoteAccountLatest(address string, currentSlot uint64) bool {
-	stmt := `SELECT slot FROM vote_account WHERE address=$1`
+	stmt := `SELECT slot FROM validator WHERE address=$1`
 	var latestSlot uint64
 	err := db.Sqlx.Get(&latestSlot, stmt, address)
 	if err != nil {
