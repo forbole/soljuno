24724cbdd6d62886a57e49f976d67a9d41ceee30
Paul <p22626262@gmail.com>
2021-12-14 15:10:05 +0800

    Build epoch update functions

--- modules/epoch/utils.go
+++ modules/epoch/utils.go
@@ -1,7 +1,57 @@
 package epoch
 
-func updateInflationRate() {}
+import (
+	"github.com/forbole/soljuno/client"
+	"github.com/forbole/soljuno/db"
+	dbtypes "github.com/forbole/soljuno/db/types"
+)
 
-func updateEpochSchedule() {}
+func updateInflationRate(epoch uint64, db db.EpochDb, client client.Proxy) error {
+	inflation, err := client.InflationRate()
+	if err != nil {
+		return err
+	}
+	return db.SaveInflationRate(
+		dbtypes.NewInflationRateRow(
+			epoch,
+			inflation.Total,
+			inflation.Foundation,
+			inflation.Validator,
+		),
+	)
+}
 
-func updateEpochInfo() {}
+func updateSupplyInfo(epoch uint64, db db.EpochDb, client client.Proxy) error {
+	supply, err := client.Supply()
+	if err != nil {
+		return err
+	}
+	return db.SaveSupplyInfo(
+		dbtypes.NewSupplyInfoRow(
+			epoch,
+			supply.Value.Total,
+			supply.Value.Circulating,
+			supply.Value.NonCirculating,
+		),
+	)
+}
+
+func updateInflationGovernorParam(epoch uint64, db db.EpochDb, client client.Proxy) error {
+	return nil
+}
+
+func updateEpochScheduleParam(epoch uint64, db db.EpochDb, client client.Proxy) error {
+	schedule, err := client.EpochSchedule()
+	if err != nil {
+		return err
+	}
+	return db.SaveEpochScheduleParam(
+		dbtypes.NewEpochScheduleParamRow(
+			epoch,
+			schedule.SlotsPerEpoch,
+			schedule.FirstNormalEpoch,
+			schedule.FirstNormalSlot,
+			schedule.Warmup,
+		),
+	)
+}
