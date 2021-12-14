b638f6e9973b12a2148decbbb50c7250838e0d02
Paul <p22626262@gmail.com>
2021-12-14 17:03:08 +0800

    Build migrations

--- db/migration/migrate.go
+++ db/migration/migrate.go
@@ -5,31 +5,61 @@ import (
 )
 
 func Up(db db.Database) error {
-	_, err := db.Exec(`DROP INDEX vote_account_node_index`)
-	if err != nil {
-		return err
-	}
-
-	_, err = db.Exec(`ALTER TABLE vote_account RENAME TO validator`)
-	if err != nil {
-		return err
-	}
-
-	_, err = db.Exec(`CREATE INDEX validator_node_index ON validator (node)`)
+	_, err := db.Exec(`CREATE TABLE epoch_info (
+		one_row_id          BOOL            NOT NULL DEFAULT TRUE PRIMARY KEY,
+		epoch               BIGINT          NOT NULL,
+		transaction_count   NUMERIC(20, 0)  NOT NULL,
+		CHECK (one_row_id)
+	);
+	
+	CREATE TABLE supply_info (
+		one_row_id      BOOL            NOT NULL DEFAULT TRUE PRIMARY KEY,
+		epoch           BIGINT          NOT NULL,
+		total           NUMERIC(20,0)   NOT NULL,
+		circulating     NUMERIC(20,0)   NOT NULL,
+		non_circulating NUMERIC(20,0)   NOT NULL,
+		CHECK (one_row_id)
+	);
+	
+	CREATE TABLE inflation_rate (
+		one_row_id  BOOL    NOT NULL DEFAULT TRUE PRIMARY KEY,
+		epoch       BIGINT  NOT NULL,
+		total       FLOAT   NOT NULL,
+		foundation  FLOAT   NOT NULL,
+		validator   FLOAT   NOT NULL,
+		CHECK (one_row_id)
+	);
+	
+	CREATE TABLE epoch_schedule_param (
+		one_row_id          BOOL    NOT NULL DEFAULT TRUE PRIMARY KEY,
+		epoch               BIGINT  NOT NULL,
+		slots_per_epoch     INT     NOT NULL,
+		first_normal_epoch  INT     NOT NULL,
+		first_normal_slot   INT     NOT NULL,
+		warmup              BOOL    NOT NULL,
+		CHECK (one_row_id)
+	);
+	
+	CREATE TABLE inflation_governor_param (
+		one_row_id          BOOL    NOT NULL DEFAULT TRUE PRIMARY KEY,
+		epoch               BIGINT  NOT NULL,
+		initial             FLOAT   NOT NULL,
+		terminal            FLOAT   NOT NULL,
+		taper               FLOAT   NOT NULL,
+		foundation          FLOAT   NOT NULL,
+		foundation_terminal FLOAT   NOT NULL,
+		CHECK (one_row_id)
+	);
+	`)
 	return err
 }
 
 func Down(db db.ExceutorDb) error {
-	_, err := db.Exec(`DROP INDEX validator_node_index`)
-	if err != nil {
-		return err
-	}
-
-	_, err = db.Exec(`ALTER TABLE validator RENAME TO vote_account`)
-	if err != nil {
-		return err
-	}
-	_, err = db.Exec(`CREATE INDEX vote_account_node_index ON vote_account (node)`)
-
+	_, err := db.Exec(`DROP TABLE epoch_info;
+	DROP TABLE supply_info;
+	DROP TABLE inflation_rate;
+	DROP TABLE epoch_schedule_param;
+	DROP TABLE inflation_governor_param;
+	`)
 	return err
 }
