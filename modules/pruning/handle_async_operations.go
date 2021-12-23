d68bd01c5a7261c4a1ba87a9b97be39394019619
Paul <p22626262@gmail.com>
2021-12-23 16:29:24 +0800

    Fix pruning slot

--- modules/pruning/handle_async_operations.go
+++ modules/pruning/handle_async_operations.go
@@ -12,10 +12,11 @@ func (m *Module) RunAsyncOperations() {
 		if err != nil {
 			log.Error().Str("module", m.Name()).Uint64("slot", block.Slot).Err(err).Send()
 		}
+		pruningSlot := block.Slot - uint64(m.cfg.GetKeepRecent())
 		for _, s := range m.services {
-			err := s.Prune(block.Slot)
+			err := s.Prune(pruningSlot)
 			if err != nil {
-				log.Error().Str("module", m.Name()).Str("target", s.Name()).Uint64("slot", block.Slot).Err(err).Send()
+				log.Error().Str("module", m.Name()).Str("target", s.Name()).Uint64("slot", pruningSlot).Err(err).Send()
 			}
 		}
 	}
