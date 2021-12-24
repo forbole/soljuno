cf33a4c6f2352945d1324c4c7a6124ab9eb47154
Paul <p22626262@gmail.com>
2021-12-24 14:24:34 +0800

    Remove duplicated functions

--- db/postgresql/solana.go
+++ db/postgresql/solana.go
@@ -210,22 +210,6 @@ func (db *Database) saveMsgAddressIndexes(msgs []types.Message) error {
 	)
 }
 
-func (db *Database) insertWithParams(insertStmt string, paramsStmt string, conflictStmt string, params []interface{}) error {
-	insertStmt += paramsStmt
-	insertStmt += conflictStmt
-	_, err := db.Sqlx.Exec(insertStmt, params...)
-	return err
-}
-
-func getParamsStmt(start, number int) string {
-	stmt := "("
-	for i := 1; i <= number; i++ {
-		stmt += fmt.Sprintf("$%d,", start+i)
-	}
-	stmt = stmt[:len(stmt)-1]
-	return stmt + "),"
-}
-
 // Close implements db.Database
 func (db *Database) Close() {
 	err := db.Sqlx.Close()
