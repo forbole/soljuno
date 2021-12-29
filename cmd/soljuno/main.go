90f08ea31fc43c7184bf94680225a37affe97338
Paul <p22626262@gmail.com>
2021-12-29 17:01:56 +0800

    Apply module register default config initially

--- cmd/soljuno/main.go
+++ cmd/soljuno/main.go
@@ -4,13 +4,14 @@ import (
 	"os"
 
 	"github.com/forbole/soljuno/cmd"
+	"github.com/forbole/soljuno/modules/registrar"
 
 	cmdtypes "github.com/forbole/soljuno/cmd/types"
 )
 
 func main() {
 	// ParsingConfig the runner
-	config := cmdtypes.NewConfig("soljuno")
+	config := cmdtypes.NewConfig("soljuno").WithRegistrar(registrar.NewDefaultRegistrar())
 
 	// Run the commands and panic on any error
 	exec := cmd.BuildDefaultExecutor(config)
