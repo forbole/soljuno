a4ae556defec4bc89a054ac914668e51ded96851
Paul <p22626262@gmail.com>
2022-01-03 14:34:07 +0800

    Add params api to proxy server

--- actions-proxy/apis/gin.go
+++ actions-proxy/apis/gin.go
@@ -10,14 +10,42 @@ import (
 )
 
 func RegisterAPIs(r *gin.Engine, proxy client.ClientProxy) {
-	r.Group("/api").
-		POST("/epoch_info", func(c *gin.Context) {
-			epochInfo, err := proxy.GetEpochInfo()
-			if err != nil {
-				c.JSON(http.StatusBadRequest, types.NewError(err))
-				return
-			}
-			c.JSON(http.StatusOK, epochInfo)
-		})
+	group := r.Group("/api")
+	group.POST("/epoch_info", func(c *gin.Context) {
+		info, err := proxy.GetEpochInfo()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, types.NewError(err))
+			return
+		}
+		c.JSON(http.StatusOK, info)
+	})
 
+	group.POST("/epoch_schedule", func(c *gin.Context) {
+		schedule, err := proxy.GetEpochSchedule()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, types.NewError(err))
+			return
+		}
+		c.JSON(http.StatusOK, schedule)
+	})
+
+	group.POST("/inflation", func(c *gin.Context) {
+		inflation, err := proxy.GetInflationRate()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, types.NewError(err))
+			return
+		}
+		c.JSON(http.StatusOK, inflation)
+	})
+
+	group.POST("/inflation_governor", func(c *gin.Context) {
+		governor, err := proxy.GetInflationGovernor()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, types.NewError(err))
+			return
+		}
+		c.JSON(http.StatusOK, governor)
+	})
+
+	group.POST("/")
 }
--- actions-proxy/apis/types/query.go
+++
@@ -1,5 +0,0 @@
-package types
-
-type Query struct {
-	GetEpochInfo EpochInfo
-}
