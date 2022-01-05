73e83e9917f59da50b22ff4a1034640e73a22698
Paul <p22626262@gmail.com>
2022-01-05 16:17:41 +0800

    Fix validator status

--- db/schema/05-vote.sql
+++ db/schema/05-vote.sql
@@ -11,13 +11,10 @@ CREATE INDEX validator_node_index ON validator (node);
 
 CREATE TABLE validator_status
 (
-    address         TEXT    NOT NULL,
+    address         TEXT    NOT NULL PRIMARY KEY,
     slot            BIGINT  NOT NULL,
     activated_stake BIGINT  NOT NULL,
     last_vote       BIGINT  NOT NULL,
     root_slot       BIGINT  NOT NULL,
-    active          BOOLEAN NOT NULL,
-    PRIMARY KEY (address, slot)
+    active          BOOLEAN NOT NULL
 );
-CREATE INDEX vote_account_voter_index ON validator_status (address);
-CREATE INDEX vote_account_slot_index ON validator_status (slot);
