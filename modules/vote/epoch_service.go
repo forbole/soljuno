2b0665fb3703921b670076448afcf7a2818691df
Paul <p22626262@gmail.com>
2022-01-17 14:24:13 +0800

    Improve calcurate skip rate function

--- modules/vote/epoch_service.go
+++ modules/vote/epoch_service.go
@@ -41,14 +41,14 @@ func (m *Module) updateValidatorSkipRates(epoch uint64) error {
 
 // CalculateSkipRate returns the skip rate of the validator from the given produced map and the validator schedule
 func CalculateSkipRate(end int, produced map[int]bool, schedule []int) float64 {
-	var producedCount float64 = 0
+	var missingCount float64 = 0
 	for _, slotInEpoch := range schedule {
 		if slotInEpoch > end {
 			break
 		}
 		if ok := produced[slotInEpoch]; !ok {
-			producedCount++
+			missingCount++
 		}
 	}
-	return 1 - (producedCount / float64(end+1))
+	return missingCount / float64(end+1)
 }
