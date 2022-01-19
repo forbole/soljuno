cd343b29d52dfbe065edd316c0f2b1ca774d1a75
Paul <p22626262@gmail.com>
2022-01-19 17:20:01 +0800

    Improve bank modules

--- modules/bank/handle_periodic_operations.go
+++ modules/bank/handle_periodic_operations.go
@@ -14,24 +14,34 @@ func (m *Module) RegisterPeriodicOperations(scheduler *gocron.Scheduler) error {
 	log.Debug().Str("module", m.Name()).Msg("setting up periodic tasks")
 
 	if _, err := scheduler.Every(10).Second().Do(func() {
-		utils.WatchMethod(m.updateBalances)
+		m.mtx.Lock()
+		balances := m.balanceEntries
+		tokenBalances := m.tokenBalanceEntries
+		m.balanceEntries = nil
+		m.tokenBalanceEntries = nil
+		m.mtx.Unlock()
+
+		utils.WatchMethod(func() error { return m.updateBalances(balances, tokenBalances) })
 	}); err != nil {
 		return fmt.Errorf("error while setting up vote periodic operation: %s", err)
 	}
-
 	return nil
 }
-func (m *Module) updateBalances() error {
-	m.mtx.Lock()
-	balances := m.balanceEntries
-	tokenBalances := m.tokenBalanceEntries
-	m.balanceEntries = nil
-	m.tokenBalanceEntries = nil
-	m.mtx.Unlock()
-	err := m.db.SaveAccountBalances(EntriesToBalances(balances))
-	if err != nil {
-		return err
+
+func (m *Module) updateBalances(balances []AccountBalanceEntry, tokenBalances []TokenAccountBalanceEntry) error {
+	errChan := make(chan error)
+	go func() {
+		errChan <- m.db.SaveAccountBalances(EntriesToBalances(balances))
+
+	}()
+	go func() {
+		errChan <- m.db.SaveAccountTokenBalances(EntriesToTokenBalances(tokenBalances))
+	}()
+	for i := 0; i < 2; i++ {
+		err := <-errChan
+		if err != nil {
+			return err
+		}
 	}
-	err = m.db.SaveAccountTokenBalances(EntriesToTokenBalances(tokenBalances))
-	return err
+	return nil
 }
