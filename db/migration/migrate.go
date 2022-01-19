6ff5bb7f8eb8fb0ca714410d0800b323757d126f
Paul <p22626262@gmail.com>
2022-01-19 18:22:54 +0800

    Update migrations

--- db/migration/migrate.go
+++ db/migration/migrate.go
@@ -6,23 +6,31 @@ import (
 
 func Up(db db.Database) error {
 	_, err := db.Exec(`
-	CREATE TABLE token_price_history
+	CREATE TABLE account_balance_history
 	(
-		id          TEXT                        NOT NULL,
-		price       DECIMAL                     NOT NULL,
-		market_cap  BIGINT                      NOT NULL,
-		symbol      TEXT                        NOT NULL,
-		timestamp   TIMESTAMP WITHOUT TIME ZONE NOT NULL
+		address     TEXT                        NOT NULL,
+		timestamp   TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+		balance     BIGINT                      NOT NULL
 	);
-	CREATE INDEX token_price_history_id_index ON token_price (id);
-	CREATE INDEX token_price_history_timestamp_index ON token_price (timestamp);
+	CREATE INDEX account_balance_history_address ON account_balance_history(address);
+	CREATE INDEX account_balance_history_timestamp ON account_balance_history(timestamp);
+	
+	CREATE TABLE token_account_balance_history
+	(
+		address     TEXT                        NOT NULL,
+		timestamp   TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+		balance     NUMERIC(20,0)               NOT NULL
+	);
+	CREATE INDEX token_account_balance_history_address ON token_account_balance_history(address);
+	CREATE INDEX token_account_balance_history_timestamp ON token_account_balance_history(timestamp);
 	`)
 	return err
 }
 
 func Down(db db.ExceutorDb) error {
 	_, err := db.Exec(`
-	DROP TABLE token_price_history;
+	DROP TABLE account_balance_history;
+	DROP TABLE token_account_balance_history;
 	`)
 	return err
 }
