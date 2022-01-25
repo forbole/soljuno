26791cfd34378e4b6899d09d77a099949e6d522c
Paul <p22626262@gmail.com>
2022-01-25 19:37:12 +0800

    Update hasura

--- db/schema/00-solana.sql
+++ db/schema/00-solana.sql
@@ -14,14 +14,13 @@ CREATE INDEX block_timestamp_index ON block (timestamp);
 
 CREATE TABLE transaction
 (
-    hash       TEXT     NOT NULL,
-    slot       BIGINT   NOT NULL,
-    error      BOOLEAN  NOT NULL,
-    fee        INT      NOT NULL,
-    logs       TEXT[],
-    messages   JSONB    NOT NULL DEFAULT '{}',
-    
-    partition_id    INT    NOT NULL,
+    hash            TEXT    NOT NULL,
+    slot            BIGINT  NOT NULL,
+    error           BOOLEAN NOT NULL,
+    fee             INT     NOT NULL,
+    logs            TEXT[],
+    messages        JSON    NOT NULL DEFAULT '{}',
+    partition_id    INT     NOT NULL,
     CHECK (slot / 1000 = partition_id)
 ) PARTITION BY LIST(partition_id);
 ALTER TABLE transaction ADD UNIQUE (hash, partition_id);
