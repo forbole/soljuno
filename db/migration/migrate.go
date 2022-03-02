840db78bd941e8b673e4631682bfcb80933d30a4
Paul <p22626262@gmail.com>
2022-03-02 17:35:05 +0800

    Reindex block timestamp

--- db/migration/migrate.go
+++ db/migration/migrate.go
@@ -6,18 +6,18 @@ import (
 
 func Up(db db.ExcecutorDb) error {
 	_, err := db.Exec(`
-	ALTER INDEX message_slot_index RENAME TO message_slot_old_index;
-	CREATE INDEX message_slot_index ON message (slot DESC);
-	DROP INDEX message_slot_old_index;
+	ALTER INDEX block_timestamp_index RENAME TO block_timestamp_old_index;
+	CREATE INDEX block_timestamp_index ON block (timestamp DESC);
+	DROP INDEX block_timestamp_old_index;
 	`)
 	return err
 }
 
 func Down(db db.ExcecutorDb) error {
 	_, err := db.Exec(`
-	ALTER INDEX message_slot_index RENAME TO message_slot_old_index;
-	CREATE INDEX message_slot_index ON message (slot);
-	DROP INDEX message_slot_old_index;
+	ALTER INDEX block_timestamp_index RENAME TO block_timestamp_old_index;
+	CREATE INDEX block_timestamp_index ON block (timestamp DESC);
+	DROP INDEX block_timestamp_old_index;
 	`)
 	return err
 }
