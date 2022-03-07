cfcaf1b8c59d5ba338251c4ba3a361bd5723f54f
Paul <p22626262@gmail.com>
2022-03-07 19:26:45 +0800

    Fix history module register function

--- modules/history/service.go
+++ modules/history/service.go
@@ -13,11 +13,16 @@ func (m *Module) RegisterService(services ...HistroyService) {
 
 func (m *Module) RunServices(scheduler *gocron.Scheduler) error {
 	for _, service := range m.services {
-		if _, err := scheduler.Cron(service.Cron()).Do(func() {
-			utils.WatchMethod(m, service.ExecHistory)
-		}); err != nil {
-			return fmt.Errorf("error while setting up history period operations: %s", err)
-		}
+		m.registerService(scheduler, service)
+	}
+	return nil
+}
+
+func (m *Module) registerService(scheduler *gocron.Scheduler, service HistroyService) error {
+	if _, err := scheduler.Cron(service.Cron()).Do(func() {
+		utils.WatchMethod(m, service.ExecHistory)
+	}); err != nil {
+		return fmt.Errorf("error while setting up history period operations: %s", err)
 	}
 	return nil
 }
