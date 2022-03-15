bed112d0cb50feddd357bb1fd278596b45beded6
Paul <p22626262@gmail.com>
2022-03-15 11:41:07 +0800

    Implement instruction count (#172)

--- db/types/transaction.go
+++ db/types/transaction.go
@@ -5,22 +5,24 @@ import (
 )
 
 type TxRow struct {
-	Signature   string      `db:"signature"`
-	Slot        uint64      `db:"slot"`
-	Error       bool        `db:"error"`
-	Fee         uint64      `db:"fee"`
-	Logs        interface{} `db:"logs"`
-	PartitionId int         `db:"partition_id"`
+	Signature       string      `db:"signature"`
+	Slot            uint64      `db:"slot"`
+	Error           bool        `db:"error"`
+	Fee             uint64      `db:"fee"`
+	Logs            interface{} `db:"logs"`
+	NumInstructions int         `db:"num_instructions"`
+	PartitionId     int         `db:"partition_id"`
 }
 
-func NewTxRow(signature string, slot uint64, isErr bool, fee uint64, logs []string) TxRow {
+func NewTxRow(signature string, slot uint64, isErr bool, fee uint64, logs []string, numInstructions int) TxRow {
 	return TxRow{
-		Signature:   signature,
-		Slot:        slot,
-		Error:       isErr,
-		Fee:         fee,
-		Logs:        logs,
-		PartitionId: int(slot / 1000),
+		Signature:       signature,
+		Slot:            slot,
+		Error:           isErr,
+		Fee:             fee,
+		Logs:            logs,
+		NumInstructions: numInstructions,
+		PartitionId:     int(slot / 1000),
 	}
 }
 
@@ -33,6 +35,7 @@ func NewTxRowsFromTxs(txs []types.Tx) ([]TxRow, error) {
 			tx.Successful(),
 			tx.Fee,
 			tx.Logs,
+			len(tx.Instructions),
 		)
 	}
 	return txRows, nil
