4c9dc9138176bbf08e9597864d794937752c25fe
Paul <p22626262@gmail.com>
2022-04-13 13:45:43 +0800

    Improve function

--- db/migration/migrate.go
+++ db/migration/migrate.go
@@ -6,14 +6,46 @@ import (
 
 func Up(db db.ExcecutorDb) error {
 	_, err := db.Exec(`
-	ALTER TABLE transaction RENAME COLUMN error To success;
+	DROP FUNCTION instructions_by_address;
+	CREATE FUNCTION instructions_by_address(
+		addresses TEXT[],
+		programs TEXT[],
+		"start_slot" BIGINT = 0,
+		"end_slot" BIGINT = 0
+		)
+		RETURNS SETOF instruction AS
+	$$
+	SELECT 
+		instruction.tx_signature, instruction.slot, instruction.index, instruction.inner_index, instruction.program, instruction.involved_accounts, instruction.raw_data, instruction.type, instruction.value, instruction.partition_id
+	FROM (
+		SELECT * FROM instruction WHERE 
+		(slot < "end_slot" AND slot >= "start_slot") AND
+		(cardinality(programs) = 0 OR program = ANY (programs)) AND 
+		involved_accounts @> addresses
+		) as instruction 
+	$$ LANGUAGE sql STABLE;
 	`)
 	return err
 }
 
 func Down(db db.ExcecutorDb) error {
 	_, err := db.Exec(`
-	ALTER TABLE transaction RENAME COLUMN success To error;
+	DROP FUNCTION instructions_by_address;
+	CREATE FUNCTION instructions_by_address(
+		addresses TEXT[],
+		programs TEXT[],
+		"limit" BIGINT = 100,
+		"offset" BIGINT = 0)
+		RETURNS SETOF instruction AS
+	$$
+	SELECT 
+		instruction.tx_signature, instruction.slot, instruction.index, instruction.inner_index, instruction.program, instruction.involved_accounts, instruction.raw_data, instruction.type, instruction.value, instruction.partition_id
+	FROM instruction
+	WHERE (cardinality(programs) = 0 OR program = ANY (programs))
+	  AND involved_accounts @> addresses
+	ORDER BY slot DESC,
+	involved_accounts LIMIT "limit" OFFSET "offset"
+	$$ LANGUAGE sql STABLE;
 	`)
 	return err
 }
--- db/schema/00-solana.sql
+++ db/schema/00-solana.sql
@@ -55,15 +55,17 @@ CREATE INDEX instruction_accounts_index ON instruction USING GIN(involved_accoun
 CREATE FUNCTION instructions_by_address(
     addresses TEXT[],
     programs TEXT[],
-    "limit" BIGINT = 100,
-    "offset" BIGINT = 0)
+    "start_slot" BIGINT = 0,
+    "end_slot" BIGINT = 0
+    )
     RETURNS SETOF instruction AS
 $$
 SELECT 
     instruction.tx_signature, instruction.slot, instruction.index, instruction.inner_index, instruction.program, instruction.involved_accounts, instruction.raw_data, instruction.type, instruction.value, instruction.partition_id
-FROM instruction
-WHERE (cardinality(programs) = 0 OR program = ANY (programs))
-  AND involved_accounts @> addresses
-ORDER BY slot DESC,
-involved_accounts LIMIT "limit" OFFSET "offset"
+FROM (
+    SELECT * FROM instruction WHERE 
+    (slot < "end_slot" AND slot >= "start_slot") AND
+    (cardinality(programs) = 0 OR program = ANY (programs)) AND 
+    involved_accounts @> addresses
+    ) as instruction 
 $$ LANGUAGE sql STABLE;
