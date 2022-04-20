1a3272d25a970b0cebef36723ea8393e50cef7cd
Paul <p22626262@gmail.com>
2022-04-20 17:29:48 +0800

    Fix migrations

--- db/migration/migrate.go
+++ db/migration/migrate.go
@@ -7,8 +7,10 @@ import (
 func Up(db db.ExcecutorDb) error {
 	_, err := db.Exec(`
 		DELETE FROM token_delegation WHERE NOT EXISTS (
-			SELECT 1 FROM token_account WHERE token_account.address = token_delegation.source_address AND
-			token_account.address = token_delegation.delegate_address
+			SELECT 1 FROM token_account WHERE token_account.address = token_delegation.source_address
+		);
+		DELETE FROM token_delegation WHERE NOT EXISTS (
+			SELECT 1 FROM token_account WHERE token_account.address = token_delegation.delegate_address
 		);
 
 		ALTER TABLE token_delegation ADD CONSTRAINT token_delegation_source_address_fk
--- db/migration/migrate_test.go
+++ db/migration/migrate_test.go
@@ -12,9 +12,23 @@ func (suite *MigrateTestSuite) TestMigrate() {
 	// custom query
 	err = suite.database.SaveTokenAccount(dbtypes.NewTokenAccountRow("source", 0, "mint", "owner"))
 	suite.Require().NoError(err)
+	err = suite.database.SaveTokenAccount(dbtypes.NewTokenAccountRow("dest", 0, "mint", "owner"))
+	suite.Require().NoError(err)
 	err = suite.database.SaveTokenDelegation(dbtypes.NewTokenDelegationRow("source", "dest", 0, 1))
 	suite.Require().NoError(err)
+	err = suite.database.SaveTokenDelegation(dbtypes.NewTokenDelegationRow("dest", "dest1", 0, 1))
+	suite.Require().NoError(err)
+
+	var count []int
+	err = suite.database.Sqlx.Select(&count, "SELECT COUNT(*) FROM token_delegation")
+	suite.Require().NoError(err)
+	suite.Require().Equal([]int{2}, count)
+	count = nil
 
 	err = migration.Up(suite.database)
 	suite.Require().NoError(err)
+
+	err = suite.database.Sqlx.Select(&count, "SELECT COUNT(*) FROM token_delegation")
+	suite.Require().NoError(err)
+	suite.Require().Equal([]int{1}, count)
 }
