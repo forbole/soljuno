48220e57268d5032d9790ba42b25afa61325271a
Paul <p22626262@gmail.com>
2022-04-28 19:02:34 +0800

    Optimize transactions_by_address

--- db/schema/00-solana.sql
+++ db/schema/00-solana.sql
@@ -38,7 +38,7 @@ CREATE TABLE transaction_by_address
     index           INT     NOT NULL DEFAULT 0,
     partition_id    INT     NOT NULL
 ) PARTITION BY LIST(partition_id);
-ALTER TABLE transaction_by_address ADD UNIQUE (address, signature, partition_id);
+ALTER TABLE transaction ADD UNIQUE (address, signature, partition_id);
 CREATE INDEX transaction_by_address_index ON transaction_by_address(address);
 CREATE INDEX transaction_by_address_signature_index ON transaction_by_address(signature);
 CREATE INDEX transaction_by_address_slot_index ON transaction_by_address (slot DESC);
@@ -76,8 +76,63 @@ CREATE FUNCTION transactions_by_address(
 $$
     SELECT * FROM transaction WHERE 
     (slot <= "end_slot" AND slot >= "start_slot") AND
-    involved_accounts @> addresses ORDER BY slot+0 DESC
+    involved_accounts @> addresses ORDER BY slot+0 DESC;
 $$ LANGUAGE sql STABLE;
+
+CREATE OR REPLACE FUNCTION transactions_by_address_2(
+    "target"    TEXT,
+    "current"   TEXT = '',
+    "limit"     INT = 10
+    )
+    RETURNS SETOF transaction AS
+$$
+    BEGIN
+        IF "current" = '' THEN
+            RETURN QUERY SELECT * FROM transaction WHERE
+            EXISTS (SELECT 1 FROM transaction_by_address WHERE address = "target") AND
+            signature IN ( 
+                SELECT signature FROM transaction_by_address WHERE address = "target" ORDER BY slot DESC, index DESC LIMIT "limit"
+            );
+        ELSE    
+            RETURN QUERY WITH slot_getter AS (
+                /* slot_filter returns the tx current slot */
+                SELECT DISTINCT(slot) FROM transaction_by_address 
+                    WHERE signature = "current"
+                ), 
+                /* index_getter returns the current tx index */
+                index_getter AS (
+                    SELECT DISTINCT(index) FROM transaction_by_address 
+                    WHERE signature = "current"
+                ),
+                /* account_checker returns if the signature exists behind the current tx slot and index */
+                account_checker AS (
+                    SELECT signature FROM transaction_by_address WHERE slot <= (
+                        SELECT slot FROM slot_getter
+                    ) AND 
+                    index < (
+                        SELECT index FROM index_getter
+                    ) AND address = "target"
+                )
+                ,
+                /* account_signatures_getter returns the signatures filtered by the account behind the current tx slot and index */
+                account_signatures_getter AS (
+                    SELECT signature FROM transaction_by_address WHERE slot <= (
+                        SELECT slot FROM slot_getter
+                    ) AND 
+                    index < (
+                        SELECT index FROM index_getter
+                    ) AND address = "target" ORDER BY slot DESC, index DESC LIMIT "limit"
+                )
+                /* main query */  
+                SELECT * FROM transaction WHERE EXISTS (
+                    SELECT 1 FROM account_checker
+                ) AND
+                signature IN ( 
+                    SELECT signature FROM account_signatures_getter
+                );
+        END IF;
+    END;
+$$ LANGUAGE plpgsql;
 
 /**
  * This function is used to find all the utils that involve any of the given addresses and have
