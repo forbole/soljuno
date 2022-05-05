2dcb4185729d60fc7a412fa169253f039c0b06f0
Paul <p22626262@gmail.com>
2022-05-05 17:59:36 +0800

    Fix migrations

--- db/migration/migrate.go
+++ db/migration/migrate.go
@@ -7,6 +7,11 @@ import (
 func Up(db db.ExcecutorDb) error {
 	_, err := db.Exec(`
 		CREATE INDEX CONCURRENTLY token_account_balance_index ON token_account_balance(balance DESC);
+	`)
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec(`
 		CREATE INDEX CONCURRENTLY token_account_mint_index ON token_account (mint);
 	`)
 	return err
