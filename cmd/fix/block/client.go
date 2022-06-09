cd2f694bbda5948546059a6a7fa8c876614d336b
Paul <p22626262@gmail.com>
2022-06-09 18:36:21 +0800

    Fix delay

--- cmd/fix/block/client.go
+++ cmd/fix/block/client.go
@@ -29,96 +29,86 @@ func delay() {
 }
 
 func (cp *proxy) GetLatestSlot() (uint64, error) {
-	defer delay()
-
 	cp.mtx.Lock()
 	defer cp.mtx.Unlock()
+	defer delay()
 	return cp.rpcClient.GetLatestSlot()
 }
 
 func (cp *proxy) GetBlocks(start uint64, end uint64) ([]uint64, error) {
-	defer delay()
-
 	cp.mtx.Lock()
 	defer cp.mtx.Unlock()
+	defer delay()
 	return cp.rpcClient.GetBlocks(start, end)
 }
 
 func (cp *proxy) GetBlock(slot uint64) (clienttypes.BlockResult, error) {
-	defer delay()
-
 	cp.mtx.Lock()
 	defer cp.mtx.Unlock()
+	defer delay()
 	return cp.rpcClient.GetBlock(slot)
 }
 
 func (cp *proxy) GetVoteAccounts() (clienttypes.VoteAccounts, error) {
 	cp.mtx.Lock()
 	defer cp.mtx.Unlock()
+	defer delay()
 	return cp.rpcClient.GetVoteAccounts()
 }
 
 func (cp *proxy) GetVoteAccountsWithSlot() (uint64, clienttypes.VoteAccounts, error) {
-	defer delay()
-
 	cp.mtx.Lock()
 	defer cp.mtx.Unlock()
+	defer delay()
 	return cp.rpcClient.GetVoteAccountsWithSlot()
 }
 
 func (cp *proxy) GetAccountInfo(address string) (clienttypes.AccountInfo, error) {
-	defer delay()
-
 	cp.mtx.Lock()
 	defer cp.mtx.Unlock()
+	defer delay()
 	return cp.rpcClient.GetAccountInfo(address)
 }
 
 func (cp *proxy) GetSupplyInfo() (clienttypes.SupplyWithContext, error) {
-	defer delay()
-
 	cp.mtx.Lock()
 	defer cp.mtx.Unlock()
+	defer delay()
 	return cp.rpcClient.GetSupplyInfo()
 }
 
 func (cp *proxy) GetInflationRate() (clienttypes.InflationRate, error) {
-	defer delay()
-
 	cp.mtx.Lock()
 	defer cp.mtx.Unlock()
+	defer delay()
 	return cp.rpcClient.GetInflationRate()
 }
 
 func (cp *proxy) GetLeaderSchedule(slot uint64) (clienttypes.LeaderSchedule, error) {
-	defer delay()
-
 	cp.mtx.Lock()
 	defer cp.mtx.Unlock()
+	defer delay()
 	return cp.rpcClient.GetLeaderSchedule(slot)
 }
 
 func (cp *proxy) GetEpochInfo() (clienttypes.EpochInfo, error) {
-	defer delay()
-
 	cp.mtx.Lock()
 	defer cp.mtx.Unlock()
+	defer delay()
 	return cp.rpcClient.GetEpochInfo()
 }
 
 func (cp *proxy) GetEpochSchedule() (clienttypes.EpochSchedule, error) {
-	defer delay()
-
 	cp.mtx.Lock()
 	defer cp.mtx.Unlock()
+	defer delay()
 	return cp.rpcClient.GetEpochSchedule()
 }
 
 func (cp *proxy) GetInflationGovernor() (clienttypes.InflationGovernor, error) {
-	defer delay()
-
 	cp.mtx.Lock()
 	defer cp.mtx.Unlock()
+	defer delay()
 	return cp.rpcClient.GetInflationGovernor()
 }
 
@@ -126,25 +116,22 @@ func (cp *proxy) GetSignaturesForAddress(
 	address string,
 	config clienttypes.GetSignaturesForAddressConfig,
 ) ([]clienttypes.ConfirmedTransactionStatusWithSignature, error) {
-	defer delay()
-
 	cp.mtx.Lock()
 	defer cp.mtx.Unlock()
+	defer delay()
 	return cp.rpcClient.GetSignaturesForAddress(address, config)
 }
 
 func (cp *proxy) GetTransaction(signature string) (clienttypes.EncodedConfirmedTransactionWithStatusMeta, error) {
-	defer delay()
-
 	cp.mtx.Lock()
 	defer cp.mtx.Unlock()
+	defer delay()
 	return cp.rpcClient.GetTransaction(signature)
 }
 
 func (cp *proxy) GetSlotLeaders(slot uint64, limit uint64) ([]string, error) {
-	defer delay()
-
 	cp.mtx.Lock()
 	defer cp.mtx.Unlock()
+	defer delay()
 	return cp.rpcClient.GetSlotLeaders(slot, limit)
 }
